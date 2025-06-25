e36cd48cccb73f1e202afb482ddb99f7972cb32b
san san <pts.20.sn@gmail.com>
2025-06-25 17:35:57 +0700

    Add Project

---
+++ VocabLearningApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VocabLearningApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
